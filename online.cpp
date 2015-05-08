9bb9e82a6b281e6fb9a10949d82a20104be7e272
whitewidowtrick <kalbert1991@gmail.com>
2015-05-07 19:02:15 -0700

    Create online.cpp

---
+++ online.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int x=0;
+  cout << endl << "Enter Value: ";
+  cin >> x;
+  cout << endl << "Value of X: " << x << endl;
+
+return 0;
+}
