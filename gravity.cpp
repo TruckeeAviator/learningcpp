a617d0a1b4b7ab723063429924a6e52113fbc8c8
Kory <kalbert1991@gmail.com>
2015-05-10 22:50:25 -0700

    Update gravity.cpp

--- gravity.cpp
+++ gravity.cpp
@@ -2,34 +2,35 @@
 #include "gravity.h"
 using namespace std;
 
-double calculate(int height, int seconds)
+double calculate(double height, int seconds)
 {
+	double distance;
 	distance=Constant::gravity * (seconds*seconds) /2;
 	return distance;
 }
 
 int main()
 {
- 	int height  = 0;
+ 	double height  = 0;
 	int seconds = 0;
 	int dropped = 0;
 
 	cout << "Enter height of the object in meters: ";
 	cin >> height;
 
-	dropped=calculate(height,seconds)-height;
+	dropped=height-calculate(height,seconds);
 	cout << "At " << seconds << " the ball dropped " << dropped << " meters." << endl;
 	seconds=seconds+1;
-	dropped=calculate(height,seconds)-height;
+	dropped=height-calculate(height,seconds);
 	cout << "At " << seconds << " the ball dropped " << dropped << " meters." << endl;
 	seconds=seconds+1;
-	dropped=calculate(height,seconds)-height;
+	dropped=height-calculate(height,seconds);
 	cout << "At " << seconds << " the ball dropped " << dropped << " meters." << endl;
 	seconds=seconds+1;
-	dropped=calculate(height,seconds)-height;
+	dropped=height-calculate(height,seconds);
 	cout << "At " << seconds << " the ball dropped " << dropped << " meters." << endl;
 	seconds=seconds+1;
-	dropped=calculate(height,seconds)-height;
+	dropped=height-calculate(height,seconds);
 	cout << "At " << seconds << " the ball dropped " << dropped << " meters." << endl;
 	seconds=seconds+1;
 }
