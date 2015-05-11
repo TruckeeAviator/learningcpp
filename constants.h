ba73c0aa933f649ca47fb026cb65a15946b86ef1
Kory <kalbert1991@gmail.com>
2015-05-10 20:19:39 -0700

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+ 
+// define your own namespace to hold constants
+namespace Constants
+{
+    const double pi(3.14159);
+    const double avogadro(6.0221413e23);
+}
+#endif
